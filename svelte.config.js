55713a4148e6d1b9a5921ef1e7ccbc52456befdc
erdavis1 <45726527+erdavis1@users.noreply.github.com>
2025-04-23 14:18:06 -0700

    still messing with base

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
 export default {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/thread-colors' 
+			base: ''  // ← important: empty string for root domain
 		}
 	}
 };
